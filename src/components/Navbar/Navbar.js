6b3a48da1b3724c3e889cc51bd71709b8070fe69
A0007 <ajaybendre@credenceanalytics.com>
2021-08-21 15:57:49 +0530

    Fixes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
       <div className="slnt absolute"></div>
       <div className="relative z-10 flex justify-between top-2 px-20">
         <img src="/static/images/csk.png" className="object-fit" />
-        <div className='bg-white rounded-full h-10 w-10 mt-4'>
+        <div className='bg-white rounded-full h-10 w-10 mt-4 mr-4'>
           <Text
             class='text-center mt-2'
             text="VS"
