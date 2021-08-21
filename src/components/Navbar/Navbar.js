231e476d381af4503f7873fbbe663ecc8973b006
A0007 <ajaybendre@credenceanalytics.com>
2021-08-21 15:54:13 +0530

    Navbar fixes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Text } from "../../common";
 
 function Navbar() {
   return (
@@ -6,6 +7,14 @@ function Navbar() {
       <div className="slnt absolute"></div>
       <div className="relative z-10 flex justify-between top-2 px-20">
         <img src="/static/images/csk.png" className="object-fit" />
+        <div className='bg-white rounded-full h-12 w-12 mt-3'>
+          <Text
+            class='text-center mt-3'
+            text="VS"
+            fontColor="#283574"
+            fontWeight="600"
+          />
+        </div>
         <img src="/static/images/rcb.png" className="object-fit" />
       </div>
     </div>
