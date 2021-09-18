a2a828584dd5d8735c2fbc81e858da2055ec8e85
Ajay 007 <ajaybendre@credenceanalytics.com>
2021-09-18 17:17:02 +0530

    circle added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Footer, Navbar } from "./components";
-import LandingScreen  from "./Pages/LandingScreen";
-
+import LandingScreen from "./Pages/LandingScreen";
 
 function App() {
+  const [show, setShow] = useState(false);
+
   return (
     <div>
-      <Navbar />
-      <LandingScreen />
-      <Footer />
+      <div className="fixed right-10 bottom-10">
+        <div
+          className={` h-14 w-14 bg-red-300 rounded-full`}
+          onClick={() => setShow(!show)}
+        ></div>
+      </div>
+      <div className={`${show ? "block" : "hidden"}`}>
+        <Navbar />
+        <LandingScreen />
+        <Footer />
+      </div>
     </div>
   );
 }
