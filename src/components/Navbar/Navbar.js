4d2d8a758cba2293fcef15d270079431dde3ed4c
Ajay 007 <ajaybendre@credenceanalytics.com>
2021-09-21 18:36:18 +0530

    clean code

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,6 @@ import { Text } from "../../common";
 import { getFirstCharacter } from "../../utils/getFirstCharacter";
 
 function Navbar({ powerStatsData }) {
-
   const [teamOne, setTeamOne] = useState("");
   const [teamTwo, setTeamTwo] = useState("");
 
@@ -12,15 +11,19 @@ function Navbar({ powerStatsData }) {
       setTeamOne(powerStatsData.TeamMatches[0].ForTeam);
       setTeamTwo(powerStatsData.TeamMatches[0].AgainstTeam);
     }
-  }, [powerStatsData])
+  }, [powerStatsData]);
   console.log(powerStatsData.TeamMatches);
 
   return (
     <div className="overflow-hidden">
       <div className="slnt absolute overflow-hidden"></div>
       <div className="relative z-10 flex justify-between top-2 sm:px-20 px-7">
-        <img src={`https://utterai.s3.ap-south-1.amazonaws.com/img/${teamOne && getFirstCharacter(teamOne)
-          }.png`} className="w-20 object-contain" />
+        <img
+          src={`https://utterai.s3.ap-south-1.amazonaws.com/img/${
+            teamOne && getFirstCharacter(teamOne)
+          }.png`}
+          className="w-20 object-contain"
+        />
         <div className="bg-white rounded-full h-10 w-10 mt-4 sm:mr-4 mr-10">
           <Text
             class="text-center mt-2"
@@ -29,8 +32,12 @@ function Navbar({ powerStatsData }) {
             fontWeight="600"
           />
         </div>
-        <img src={`https://utterai.s3.ap-south-1.amazonaws.com/img/${teamTwo && getFirstCharacter(teamTwo)
-          }.png`} className="w-20 object-contain" />
+        <img
+          src={`https://utterai.s3.ap-south-1.amazonaws.com/img/${
+            teamTwo && getFirstCharacter(teamTwo)
+          }.png`}
+          className="w-20 object-contain"
+        />
       </div>
     </div>
   );
