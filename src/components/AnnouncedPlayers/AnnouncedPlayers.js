c25128f0c5dea4ca693fada0c9b57cebb3ab2232
Ajay 007 <ajaybendre@credenceanalytics.com>
2021-09-22 12:12:20 +0530

    final changes

--- src/components/AnnouncedPlayers/AnnouncedPlayers.js
+++ src/components/AnnouncedPlayers/AnnouncedPlayers.js
@@ -15,7 +15,7 @@ const AnnouncedPlayers = (props) => {
     setActiveTeamData(anouncedPlayers.iplt20_2021_g032[teamName]);
     setBtn(flag, teamName);
     selectedTeam = index;
-  }
+  };
   const [ws, setWS] = useState(null);
   const [teamsdata, setTeamsData] = useState([]);
 
@@ -23,18 +23,25 @@ const AnnouncedPlayers = (props) => {
     axios
       .post("https://hapi.utter.ai/api/v1.0/getCurrentPlayingXI", null, {
         headers: {
-          // Authorization: `Bearer ${window.utter_token}`
-          Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJVdHRlckFJIiwidXNlciI6eyJ1c2VybmFtZSI6IndlYnBsYXRmb3JtQVBJIiwicm9sZSI6InJlc3RDbGllbnQifSwiaWF0IjoxNjMyMjQ2NTY0LCJpZCI6IlFkZlRyMDM0NEdkdzhibSIsImV4cCI6MTYzMjMzMjk2NH0.AzkorK0AZWEw1XaSp-Q34qjMAEqRbEG_l3nMeibC_WM'}`
+          Authorization: `Bearer ${window.utter_token}`
         }
       })
       .then((results) => {
         for (var match_key in results.data.current_match_playingxi) {
-          matchKey= match_key;
+          matchKey = match_key;
           var teams = results.data.current_match_playingxi[match_key];
           if (Object.keys(teams).length) {
             //means teams is defined and player list is there
-            setTeamsData(Object.keys(results.data.current_match_playingxi[matchKey]));
-            setActiveTeamData(results.data.current_match_playingxi[matchKey][Object.keys(results.data.current_match_playingxi[matchKey])[selectedTeam]]);
+            setTeamsData(
+              Object.keys(results.data.current_match_playingxi[matchKey])
+            );
+            setActiveTeamData(
+              results.data.current_match_playingxi[matchKey][
+                Object.keys(results.data.current_match_playingxi[matchKey])[
+                  selectedTeam
+                ]
+              ]
+            );
             setAnouncedPlayers(results.data.current_match_playingxi);
           } else {
             //means empty object
@@ -47,14 +54,15 @@ const AnnouncedPlayers = (props) => {
 
   const createWebScoket = (url) => {
     let ws = new WebSocket(url);
-    ws.onopen = () => {
-    };
+    ws.onopen = () => {};
 
     ws.onmessage = (evt) => {
       var announced_players_data;
       let isMatched = null;
-      if (!!localStorage.getItem('announced_players_data')) {
-        announced_players_data = JSON.parse(localStorage.getItem('announced_players_data'));
+      if (!!localStorage.getItem("announced_players_data")) {
+        announced_players_data = JSON.parse(
+          localStorage.getItem("announced_players_data")
+        );
         if (Object.keys(announced_players_data).includes(matchKey)) {
           isMatched = true;
         } else {
@@ -63,10 +71,16 @@ const AnnouncedPlayers = (props) => {
       }
       if (isMatched) {
         setTeamsData(Object.keys(announced_players_data[matchKey]));
-        setActiveTeamData(announced_players_data[matchKey][Object.keys(announced_players_data[matchKey])[selectedTeam]]);
+        setActiveTeamData(
+          announced_players_data[matchKey][
+            Object.keys(announced_players_data[matchKey])[selectedTeam]
+          ]
+        );
       } else {
-        localStorage.setItem('announced_players_data', evt.data);
-        var announced_players_data = JSON.parse(localStorage.getItem('announced_players_data'));
+        localStorage.setItem("announced_players_data", evt.data);
+        var announced_players_data = JSON.parse(
+          localStorage.getItem("announced_players_data")
+        );
         setAnouncedPlayers(announced_players_data);
         if (Object.keys(announced_players_data).includes(matchKey)) {
           isMatched = true;
@@ -75,14 +89,20 @@ const AnnouncedPlayers = (props) => {
         }
         if (isMatched) {
           setTeamsData(Object.keys(announced_players_data[matchKey]));
-          setActiveTeamData(announced_players_data[matchKey][Object.keys(announced_players_data[matchKey])[selectedTeam]]);
+          setActiveTeamData(
+            announced_players_data[matchKey][
+              Object.keys(announced_players_data[matchKey])[selectedTeam]
+            ]
+          );
         } else {
           setTeamsData([]);
           setActiveTeamData([]);
         }
       }
-      localStorage.setItem('announced_players_data', evt.data);
-      var announced_players_data = JSON.parse(localStorage.getItem('announced_players_data'));
+      localStorage.setItem("announced_players_data", evt.data);
+      var announced_players_data = JSON.parse(
+        localStorage.getItem("announced_players_data")
+      );
       setAnouncedPlayers(announced_players_data);
       if (Object.keys(announced_players_data).includes(matchKey)) {
         isMatched = true;
@@ -91,7 +111,11 @@ const AnnouncedPlayers = (props) => {
       }
       if (isMatched) {
         setTeamsData(Object.keys(announced_players_data[matchKey]));
-        setActiveTeamData(announced_players_data[matchKey][Object.keys(announced_players_data[matchKey])[selectedTeam]]);
+        setActiveTeamData(
+          announced_players_data[matchKey][
+            Object.keys(announced_players_data[matchKey])[selectedTeam]
+          ]
+        );
       } else {
         setTeamsData([]);
         setActiveTeamData([]);
@@ -106,7 +130,6 @@ const AnnouncedPlayers = (props) => {
     return ws;
   };
 
-
   return (
     <div className="p-2 py-2 rounded-md space-y-2 sm:h-72">
       {!!props.hideTitle ? (
@@ -119,36 +142,40 @@ const AnnouncedPlayers = (props) => {
           fontColor="#283574"
         />
       )}
-      {
-        teamsdata.length > 0 ?
-          <div className="flex justify-center -space-x-2">
-            <button
-              onClick={() => handleBtnClick(true, teamsdata[0], 0)}
-              className={`bg-gray-100 w-32  ${btn && "shadow-md border z-10 rounded-md"
-                } `}
-            >
-              <Text
-                fontFamily="Roboto Condensed"
-                class="text-sm font-bold"
-                text={teamsdata[0] || ""}
-                fontColor="#283574"
-              />
-            </button>
-            <button
-              onClick={() => handleBtnClick(false, teamsdata[1], 1)}
-              className={`bg-gray-100 w-32  rounded-md ${!btn && "shadow-md border z-10 rounded-md"
-                } `}
-            >
-              <Text
-                fontFamily="Roboto Condensed"
-                class="text-sm font-bold"
-                text={teamsdata[1] || ""}
-                fontColor="#656666"
-              />
-            </button>
-          </div> : <div className="text-center  text-gray-500 text-sm font-bold">Yet to be announced</div>
-      }
-
+      {teamsdata.length > 0 ? (
+        <div className="flex justify-center -space-x-2">
+          <button
+            onClick={() => handleBtnClick(true, teamsdata[0], 0)}
+            className={`bg-gray-100 w-32  ${
+              btn && "shadow-md border z-10 rounded-md"
+            } `}
+          >
+            <Text
+              fontFamily="Roboto Condensed"
+              class="text-sm font-bold"
+              text={teamsdata[0] || ""}
+              fontColor="#283574"
+            />
+          </button>
+          <button
+            onClick={() => handleBtnClick(false, teamsdata[1], 1)}
+            className={`bg-gray-100 w-32  rounded-md ${
+              !btn && "shadow-md border z-10 rounded-md"
+            } `}
+          >
+            <Text
+              fontFamily="Roboto Condensed"
+              class="text-sm font-bold"
+              text={teamsdata[1] || ""}
+              fontColor="#656666"
+            />
+          </button>
+        </div>
+      ) : (
+        <div className="text-center  text-gray-500 text-sm font-bold">
+          Yet to be announced
+        </div>
+      )}
 
       <div className="grid grid-cols-3 space-x-2 space-y-2 sm:space-y-4 sm:px-4 sm:space-x-4">
         {activeTeamData.map((el, index) => (
