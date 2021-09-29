14c840aaec7e4d5d2c7b801175730f413034e29a
velomad <sagaryc111@gmail.com>
2021-09-29 19:29:58 +0530

    token added

--- src/components/TossInsights/TossInsights.js
+++ src/components/TossInsights/TossInsights.js
@@ -10,7 +10,7 @@ const TossInsights = (props) => {
     axios
       .post("https://hapi.utter.ai/api/v1.0/getCurrentMatchToss", null, {
         headers: {
-          Authorization: `Bearer ${'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJVdHRlckFJIiwidXNlciI6eyJ1c2VybmFtZSI6IndlYnBsYXRmb3JtQVBJIiwicm9sZSI6InJlc3RDbGllbnQifSwiaWF0IjoxNjMyNzU5ODk3LCJpZCI6IlFkZlRyMDM0NEdkdzhibSIsImV4cCI6MTYzMjg0NjI5N30.8pVRYzHJYpJAuwI6MyCfEr8raWYNkOQWgtR54b1Ozhc'}`
+          Authorization: `Bearer ${window.utter_token}`
         }
       })
       .then((results) => {
